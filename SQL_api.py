325d39db8dd4121aa084ed21c6f0ca8ab2eab5eb
SecurityTCOE <137145979+SecurityTCOE@users.noreply.github.com>
2024-08-06 15:06:33 +0530

    Update SQL_api.py

--- SQL_api.py
+++ SQL_api.py
@@ -1,66 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
-import urllib.parse
-import html
-import base64
 from sklearn.feature_extraction.text import TfidfVectorizer
 from waitress import serve
 
 app = Flask(__name__)
 CORS(app)  # Allow Cross-Origin Resource Sharing
 
-# Load the trained models and TF-IDF vectorizers for SQL injection detection
+# Load the trained models and TF-IDF vectorizers for SQL injection, XSS, and HTML injection detection
 model_sql = joblib.load("best_model_sql_1,48,000 payloads.pkl")
 
-def decode_ascii_hex(encoded):
-    try:
-        return bytes.fromhex(encoded).decode('utf-8')
-    except:
-        return encoded
-
-def decode_payload(payload, max_iterations=50):
-    for _ in range(max_iterations):
-        decoded_payload = payload
-
-        # Decode URL encoded payloads
-        decoded_payload = urllib.parse.unquote(decoded_payload)
-        
-        # Decode HTML entities
-        decoded_payload = html.unescape(decoded_payload)
-        
-        # Decode Base64 encoded payloads
-        try:
-            decoded_payload = base64.b64decode(decoded_payload).decode('utf-8')
-        except:
-            pass  # Ignore errors if decoding fails
-        
-        # Decode ASCII HEX
-        decoded_payload = decode_ascii_hex(decoded_payload)
-        
-        if decoded_payload == payload:
-            break
-        
-        payload = decoded_payload
-
-    return payload
-
-@app.route('/sql_injection', methods=['POST'])
+@app.route('/sql injection', methods=['POST'])
 def detect_injections_api():
     username = request.json.get('username')
     password = request.json.get('password')
     
-    # Decode the payloads
-    decoded_username = decode_payload(username)
-    decoded_password = decode_payload(password)
-    
     # Predict SQL injection for username and password
-    prediction_sql_username = model_sql.predict([decoded_username.lower()])
-    prediction_sql_password = model_sql.predict([decoded_password.lower()])
+    prediction_sql_username = model_sql.predict([username.lower()])
+    prediction_sql_password = model_sql.predict([password.lower()])
 
     response = {
         "username_is_sql_injection": bool(prediction_sql_username),
         "password_is_sql_injection": bool(prediction_sql_password),
+
     }
 
     if response["username_is_sql_injection"] or response["password_is_sql_injection"]:
